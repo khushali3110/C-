2a74d55dd4f34cad7cb32880dfbecbe043e891fb
khushali3110 <jikadrakhushali@gmail.com>
2025-01-21 00:35:10 +0530

    code

--- Array/code.cpp
+++ Array/code.cpp
@@ -1,13 +1,10 @@
+// &-addres suggest krse
 // #include<iostream>
 // using namespace std;
 
-
-
 // int main(){
 //   int n = 10;
-
 //   int arr[n];
-
 //   arr[0] = 10;
 //   arr[1] = 20;
 //   arr[2] = 30;
@@ -39,24 +36,15 @@
 //   cout<<&arr[8]<<endl;
 //   cout<<arr[9]<<endl;
 //   cout<<&arr[9]<<endl;
-
-
-
-
-
 // }
 
 
-// &-addres suggest krse
+
 // #include<iostream>
 // using namespace std;
-
-
-
 // int main(){
-//   int n = 10;
 
-//   int arr[n] ={10,20,30,40,50,60,70,80,90,100};
+//   int arr[10] = {10,20,30,40,50,60,70,80,90,100};
 
 //   cout<<arr[0]<<endl;
 //   cout<<arr[1]<<endl;
@@ -68,22 +56,21 @@
 //   cout<<arr[7]<<endl;
 //   cout<<arr[8]<<endl;
 //   cout<<arr[9]<<endl;
-
 // }
 
-// #include<iostream>
-// using namespace std;
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n = 10;
+
+  int arr[n] ={10,20,30,40,50,60,70,80,90,100};
+
+  for(int i = 0; i < n; i++){
+    cout<<arr[i]<<endl;
+    // cout<<"Element present at "<<i<<" index : "<<arr[i]<<endl;
+  }
 
 
-
-// int main(){
-//   int n = 10;
-
-//   int arr[n] ={10,20,30,40,50,60,70,80,90,100};
-
-//   for(int i = 0; i < n; i++){
-//     cout<<"Element present at "<<i<<" index : "<<arr[i]<<endl;
-//   }
-
-
-// }
+}
